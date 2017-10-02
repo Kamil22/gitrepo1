f8ee93422d9a72192a73acfd20788f9c6a91582e
Kamil22 <pumba-wielki@wp.pl>
2017-10-02 12:35:26 +0200

    pętla for

---
+++ sumuj.cpp
@@ -0,0 +1,28 @@
+/*
+ * sumuj.cpp
+ * Program pobiera i sumuje 10 liczb, wynik  dukuje na ekranie 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int i;
+    int j;
+    for (i = 0; i < 101; i++)
+    {
+        cout << "*";
+        if (i % 10 == 0 )
+        {
+            for (j=0; j<9; j++)
+                cout << "*";
+            cout << endl;
+        }
+    }
+    
+    return 0;
+}
+
