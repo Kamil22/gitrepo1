2f9a9681cb51aead1bb9353359c7b132e3256c12
Kamil22 <pumba-wielki@wp.pl>
2017-10-09 20:00:29 +0200

    cwiczenia

---
+++ petle_cw4.cpp
@@ -0,0 +1,26 @@
+/*
+ * petle_cw4cpp
+
+ */
+
+
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+
+{
+    int i;
+    for (i=10; i<100; i++)
+    {
+    if (i%6==0)
+        {
+        cout<<i<<endl;
+    }
+}
+	
+	return 0;
+}
+
