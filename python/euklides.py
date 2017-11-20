6c692bd267fa63a06a20e3b982254df9799de183
Kamil22 <pumba-wielki@wp.pl>
2017-11-20 22:23:43 +0100

    nwd

--- python/euklides.py
+++ python/euklides.py
@@ -1,22 +1,21 @@
 
-
-def nwd(a,b):
-
-    while a!=b:
-        if a>b:
-            a=a-b
+def nwd_v1(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
         else:
-            b=b-a
-            return a
+            b = b - a
+    return a
 
-def main (args):
 
-    a=int(input('Podaj liczbę naturalną: '))
-    b=int(input('Podaj liczbe naturalną: '))
+def main(args):
+    a = int(input("Podaj liczbe naturalną: "))
+    b = int(input("podaj liczbe naturalną: "))
+    assert nwd_v1(5, 10) == 5
+    assert nwd_v1(33, 11) == 11
+    print("NWD({:d}, {:d}) = {:d}".format(a, b, nwd_v1(a, b)))
+    return 0
 
-print ('Największy wspolny dzielnik', nwd(a,b) )
-
-return 0
 
 if __name__ == '__main__':
     import sys
