1efc27eaa3d81100762ab9f958d06775690933b1
Kamil22 <pumba-wielki@wp.pl>
2017-12-11 12:34:38 +0100

    potęga.py

--- python/potęga2.py
+++ python/potęga2.py
@@ -2,31 +2,33 @@
 # -*- coding: utf-8 -*-
 #
 #  potęga2.py
-#  
-def potega(a, b):
+#
+def potega(a,b):
     wynik = 1
-    while (b>0)
+    while b > 0:
         wynik = a ** b
-        return wynik 
+    return wynik
+
+
+#  potega_rek (a, 0) = 1 dla a różnego od 0
+#  potega_rek(a, n) = potega_rek(n - 1) * a dla n = N+
+
+def potega_rek(a, n):
+    if n == 0:
+        return 1
+    return potega_rek(n - 1) * a
+
 
 def main(args):
-    a=int(input('Podaj podstawe'))
-    b=int(input('Podaj wykładnik'))
-    
-    assert type (a) == int
-    assert type (b) == int
-    
-    
-    
-while b < 0:
-    wynik a ** b
-    
-    assert potega () ==
-    
+    a = int(input('Podaj podstawę '))
+    b = int(input('Podaj wykładnik '))
+
+#  while b < 0:
+#  wynik = a ** b
+
     print (a ** b)
-    
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
