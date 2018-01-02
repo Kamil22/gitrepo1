8093d6916b75fe453aa92700a7cc0395c99c5849
Kamil22 <pumba-wielki@wp.pl>
2018-01-02 10:36:04 +0100

    sort przez wstaw

---
+++ python/sort_przez wstaw_.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  sort_przez wstaw_.py
+
+
+def sort_przez_wstaw(lista):
+    for i in range(1, len(lista)):
+        el = lista[i]
+        k = i - 1
+        while k >= 0 and lista[k] > el:
+            lista[k+1] = lista[k]
+            k -= 1
+        lista [k + 1] = el
+    return lista
+        
+
+def main(args):
+    lista = [4, 3, 7, 0, 3, 4, 1, 9]
+    print(lista)
+    print(sort_przez_wstaw(lista))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
