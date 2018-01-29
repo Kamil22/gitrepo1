6c68d2e485bdd6bd210e427cd235d2ca0f31aa31
Kamil22 <pumba-wielki@wp.pl>
2018-01-29 11:56:01 +0100

    zad

---
+++ Zyla_alg1.cpp
@@ -0,0 +1,27 @@
+/*
+ * Zyla_alg1.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    int a;
+    cout << "Wprowadz liczbe:";
+	cin >> a;
+    for (a = 0 ; a < 100; a++ ) 
+    
+    { 
+        cout << a << endl;
+       
+    }
+    
+    
+	return 0;
+}
+
