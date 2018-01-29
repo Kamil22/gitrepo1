c1a2c87fe94aab045ee600f14dcebee50ab34452
Kamil22 <pumba-wielki@wp.pl>
2018-01-29 12:33:40 +0100

    konwesja

---
+++ python/konwersje.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  konwersje.py
+#  
+
+def konwersja1(liczba10, podstawa):
+    """ Funkcja zamienia liczbe dziesietną na system o podanej podstawie"""
+    liczba = []  # lista reszt
+    while liczba10 != 0:
+        reszta = liczba10 % podstawa  # obliczanie reszt z dzielenia
+        if reszta > 9:
+            reszta = chr(reszta + 55)
+        liczba.append(str(reszta))
+        liczba10 = int(liczba10 / podstawa)
+
+    liczba.reverse()  # odwrócenie kolejności elementów! ! !
+    return "".join(liczba)
+
+
+def dec2other():
+    """Funkcja pobiera liczbę i podstawe od użytkownika """
+    liczba = int(input("Podaj liczbę: "))
+    podstawa = int(input("Podaj podstawę: "))
+    while podstawa < 2 or podstawa > 16:
+        podstawa = int(input("Podaj podstawę: "))
+    print("Wynik konwersji: {}(10) = {} ({}) ". format(
+        liczba, konwersja1(liczba, podstawa), podstawa))
+
+
+def main(args):
+    print("Zamiana liczby dziesiętnej na liczbe o podanej podstawie"
+          "<2;16> lub odwrotnie")
+    dec2other()
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
